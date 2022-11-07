32a0ed5bb3c722f0c4357d87c149fd2a739aebf5
aty2711 <101339526+aty2711@users.noreply.github.com>
2022-11-07 17:54:08 +0800

    Update User.java

--- ClassFiles/User.java
+++ ClassFiles/User.java
@@ -1,3 +1,5 @@
+
+import enums.UserType;
 public class User extends Person{
   
   
@@ -8,7 +10,26 @@ public class User extends Person{
   
   
   //get set
+  public void setPassword(String password){
+    this.password = password;
+  }
   
+  public void setUsername(String username){
+    this.username = username;
+  }
+    
+  public void setUserType(UserType usertype){
+    this.UserType = usertype;
+  }
+
+    public UserType getUserType(){
+    return this.UserType;
+  }
   //methods
   
+  public User(String firstName, String lastName){
+    super(firstName,lastName);
+    this.username = firstName + lastName;
+    this.password = "11111"; //default password
+    }
   
