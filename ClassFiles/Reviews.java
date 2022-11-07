ad7d7c99c032d6254984a25267f182d6514bfc2a
Justin <103985506+speaklouderplease@users.noreply.github.com>
2022-11-07 15:53:57 +0800

    Add files via upload
    Review interface. Interface to achieve decoupling and since it will be used directly.

---
+++ ClassFiles/Reviews.java
@@ -0,0 +1,41 @@
+public interface Reviews {
+    // public float rating;
+    // static ArrayList<String> allReviews;
+    // static ArrayList<Float> allRatings;
+      
+    /** 
+     * @return String
+     * post all reviews
+     */
+    public static void getDescription(){
+        // for(String i: allReviews){
+        //     System.out.println(i);
+        // }
+    }
+
+    /** 
+     * @param review
+     * allow user to add a written review
+     */
+    public static void setDescription(){}
+        // allReviews.add(description);
+
+    /**
+     * @param rate
+     * to collate all ratings inside a resizeable array.
+     */
+    public static void addRating(){}
+        // allRatings.add(rate);
+
+
+    /**
+     * calculates the average rating by taking the mean of all the ratings stored and 
+     * modified the rating.
+     */
+    public static void setReview(){}
+        // float sum = 0;
+        // for(float i: allRatings){
+        //     sum += i;
+        // }
+        // rating = sum/allRatings.size();
+}
