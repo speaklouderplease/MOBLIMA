f5ccbd970838bdc9cd687d38a46ed93e2128eb58
aty2711 <101339526+aty2711@users.noreply.github.com>
2022-11-10 03:51:02 +0800

    Update Pricing.java

--- ClassFiles/Pricing.java
+++ ClassFiles/Pricing.java
@@ -8,10 +8,15 @@ public class Pricing{
 //maybe method should change to getprice of all ticket member, not 1 by 1
   public double getPrice(Day day,AgeGroup age){
     double normalPrice = 8;
-    double changePrice = 0;
+    double finalPrice = 0;
    //if day = fri sat sun holi, plus extra
-   if (day == Day.
+   if (day == Day.FRIDAY || day == day.
     
     
    //if senior / child, discount
-   if (age == AgeGroup.SENIOR || age == AgeGroup.
+   if (age == AgeGroup.SENIOR || age == AgeGroup.CHILD){
+    finalPrice = 
+
+  
+  return finalPrice;
+  }
