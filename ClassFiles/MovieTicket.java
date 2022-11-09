d7e6ea266455944d867d83167de56408ebadd55f
aty2711 <101339526+aty2711@users.noreply.github.com>
2022-11-09 22:51:54 +0800

    Update MovieTicket.java

--- ClassFiles/MovieTicket.java
+++ ClassFiles/MovieTicket.java
@@ -5,4 +5,36 @@ public class MovieTicket{
   private int ticketID;
   private int seatNumber;
   private int ticketPrice;
+  private int date; //ddmmyy
+  private String showTime; 
+
+    
+  
+  //const  
+    public MovieTicket(int ticketID,int seatNumber,int date, String showTime){
+      this.ticketID = ticketID;
+      this.seatNumber = seatNumber;
+      this.date = date;
+      this.showTime = showTime;
+    }
+  
+  //get
+  
+    public int getticketID(){
+      return this.ticketID;
+    }
+  
+    public int getseatNumber()
+      return this.seatNumber;
+    }
+      
+    public int getticketPrice(){
+      return this.ticketPrice;
+    }
+
+    public int getdate(){
+      return this.date
+    }
+  
+}
   
