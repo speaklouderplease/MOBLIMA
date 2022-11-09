9dd689b0d6093b3bee1dd8d1fca8908f54ca5bf4
aty2711 <101339526+aty2711@users.noreply.github.com>
2022-11-09 23:47:21 +0800

    Create Pricing.java

---
+++ ClassFiles/Pricing.java
@@ -0,0 +1,17 @@
+import enums.AgeGroup
+import enums.Day
+  
+  
+  
+public class Pricing{
+//method to get price of 1 ticket
+//maybe method should change to getprice of all ticket member, not 1 by 1
+  public double getPrice(Day day,AgeGroup age){
+    double normalPrice = 8;
+    double changePrice = 0;
+   //if day = fri sat sun holi, plus extra
+   if (day == Day.
+    
+    
+   //if senior / child, discount
+   if (age == AgeGroup.SENIOR || age == AgeGroup.
